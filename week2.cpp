495522f405edb7195aa6a3e590eb9146e36fbc85
Jatin <bali0051@algonquinlive.com>
2025-05-10 15:42:21 -0400

    Finished constructors

--- week2.cpp
+++ week2.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    auto lang = "C++";
-    cout << "Hello and welcome to " << lang << "!\n";
+namespace CST8219 {
+    class NumberRange {
+    private:
+        int min;
+        int max;
 
-    for (int i = 1; i <= 5; i++) {
-        cout << "i = " << i << endl;
-    }
+    public:
+        // 2-parameter constructor
+        NumberRange(int _min, int _max);
+
+        // 1-parameter constructor
+        NumberRange(int _max);
+
+        // 0-parameter constructor
+        NumberRange();
+
+        // Destructor
+        ~NumberRange();
+    };
+}
+
+
+int main() {
+    // Demo #3 - Constructor with 2 parameters
+    CST8219::NumberRange five(1, 6);  // Step 11
+
+    // Demo #2 - Constructor with 1 parameter
+    // CST8219::NumberRange rangeTen(10);  // Step 9
+
+    // Demo #1 - Constructor with 0 parameters
+    // CST8219::NumberRange firstRange;    // Step 7
 
     return 0;
 }
