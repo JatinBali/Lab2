f9045229f49ced72f5bf2632e159b20ec1bfe994
Jatin <bali0051@algonquinlive.com>
2025-05-14 14:48:39 -0400

    Added NumberRange class declaration in CST8219 namespace

--- week2.cpp
+++ week2.cpp
@@ -10,6 +10,14 @@
 #include <iostream>
 using namespace std;               // allows use of cout, endl without std:: :contentReference[oaicite:0]{index=0}
 
+namespace CST8219 {
+    class NumberRange {
+    private:
+        int min, max;
+    public:
+        ~NumberRange();
+    };
+}
 int main() {
     cout << "Week 2 Lab Start" << endl;
     return 0;
