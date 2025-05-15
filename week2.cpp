0d15c2e26c0a6aa096ea751c9def236006a66b35
Jatin <bali0051@algonquinlive.com>
2025-05-15 07:34:25 -0400

    Finsihed constructors

--- week2.cpp
+++ week2.cpp
@@ -15,8 +15,34 @@ namespace CST8219 {
     private:
         int min, max;
     public:
+        NumberRange(int _min, int _max);
+
+        NumberRange(int _max);
+
+        NumberRange();
+
         ~NumberRange();
     };
+
+    // 2-parameter constructor
+    NumberRange::NumberRange(int _min, int _max) : min(_min), max(_max) {
+        cout << "In constructor with 2 parameters, min = " << min << endl;
+    }
+
+    // 1-parameter constructor
+    NumberRange::NumberRange(int _max) : NumberRange(0, _max) {
+        cout << "In constructor with 1 parameters, max = " << max << endl;
+    }
+
+    // 0-paramter constructor
+    NumberRange::NumberRange() : NumberRange(1) {
+       cout << "In constructor with 0 parameters" << endl;
+    }
+
+    // Destructor
+    NumberRange::~NumberRange() {
+        cout << "In destructor" << endl;
+    }
 }
 int main() {
     cout << "Week 2 Lab Start" << endl;
