ecd4c7b9f1ecb904fb769790457befa5a61d875b
Jatin <bali0051@algonquinlive.com>
2025-05-15 08:29:25 -0400

    Demo #1

--- week2.cpp
+++ week2.cpp
@@ -10,9 +10,40 @@
 #include <iostream>
 using namespace std;
 
+namespace CST8219 {
+    class NumberRange {
+    private:
+        int min, max;
+    public:
+        NumberRange(int _min, int _max);
+        NumberRange(int _max);
+        NumberRange();
+        ~NumberRange();
+    };
+
+    // 2-parameter constructor
+    NumberRange::NumberRange(int _min, int _max) : min(_min), max(_max) {
+        cout << "In constructor with 2 parameters, min = " << min << endl;
+    }
+
+    // 1-parameter constructor
+    NumberRange::NumberRange(int _max) : NumberRange(0, _max) {
+        cout << "In constructor with 1 parameters, max = " << max << endl;
+    }
+
+    // 0-parameter constructor
+    NumberRange::NumberRange() : NumberRange(1) {
+        cout << "In constructor with 0 parameters" << endl;
+    }
+
+    // Destructor
+    NumberRange::~NumberRange() {
+        cout << "In destructor" << endl;
+    }
+}
 
 int main() {
     cout << "Week 2 Lab Start" << endl;
-    CST8219::NumberRange rangeTen(10); // Calls 1-parameter constructor
+    CST8219::NumberRange firstNumber; // Calls 0-parameter constructor
     return 0;
 }
